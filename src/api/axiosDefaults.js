1b15db4a4c48752a23217eb971c6739622a3a2eb
Martin Bradbury <martinbradbury@btinternet.com>
2024-06-02 12:49:53 +0000

    created a signup form and sends errors back to user

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
 
-axios.defaults.baseURL = 'https://drf-api-first-6085c222ff08.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true 
+axios.defaults.baseURL = "https://drf-api-first-6085c222ff08.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
