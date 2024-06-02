d3f805975ae6a630aa14b1d229c9552557e14647
Martin Bradbury <martinbradbury@btinternet.com>
2024-06-02 19:13:49 +0000

    prettier on code.

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
   return (
-        <div className={styles.App}>
-          <NavBar />
-          <Container className={styles.Main}>
-            <Switch>
-              <Route exact path="/" render={() => <h1>Home Page</h1>} />
-              <Route exact path="/signin/" render={() => <SignInForm />} />
-              <Route exact path="/signup/" render={() => <SignUpForm />} />
-              <Route render={() => <h1>Page Not Found!</h1>} />
-            </Switch>
-          </Container>
-        </div>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home Page</h1>} />
+          <Route exact path="/signin/" render={() => <SignInForm />} />
+          <Route exact path="/signup/" render={() => <SignUpForm />} />
+          <Route render={() => <h1>Page Not Found!</h1>} />
+        </Switch>
+      </Container>
+    </div>
   );
 }
 
