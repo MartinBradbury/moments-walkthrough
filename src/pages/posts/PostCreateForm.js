e904a10be943b73b3b4c9516a0020dfac6fbc6ca
Martin Bradbury <martinbradbury@btinternet.com>
2024-06-03 22:04:51 +0000

    added logic to send image to api, error messages

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -12,11 +12,16 @@ import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import Asset from "../../components/Asset";
-import { Image } from "react-bootstrap";
+import { Alert, Image } from "react-bootstrap";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
 
 
 function PostCreateForm() {
 
+
+  const imageInput = useRef(null)  
+  const history = useHistory()
   const [errors, setErrors] = useState({});
   const [postData, setPostData] = useState({
     title: '',
@@ -41,10 +46,29 @@ const handleChangeImage = (event) => {
         })
     }
 }
+const handleSubmit = async (event) => {
+    event.preventDefault()
+    const formData = new FormData();
+        formData.append('title', title)
+        formData.append('content', content)
+        formData.append('image', imageInput.current.files[0])
+        try {
+            const {data} = await axiosReq.post('/posts/', formData)
+            history.push(`posts/${data.id}`)
+        }catch(err) {
+            if(err.response?.status !== 401)
+                setErrors(err.response?.data)
+        }
+    }
   const textFields = (
     <div className="text-center">
         <Form.Group>
             <Form.Label>Title</Form.Label>
+            {errors.title?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
             <Form.Control
                 type="text"
                 name="title"
@@ -54,6 +78,11 @@ const handleChangeImage = (event) => {
         </Form.Group>
         <Form.Group>
             <Form.Label>Description</Form.Label>
+            {errors.content?.map((message, idx) => (
+            <Alert variant="warning" key={idx}>
+            {message}
+            </Alert>
+        ))}
             <Form.Control
                 as='textarea'
                 rows={6}
@@ -67,7 +96,7 @@ const handleChangeImage = (event) => {
     
       <Button
         className={`${btnStyles.Button} ${btnStyles.Blue}`}
-        onClick={() => {}}
+        onClick={() => history.goBack()}
       >
         cancel
       </Button>
@@ -78,7 +107,7 @@ const handleChangeImage = (event) => {
   );
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Row>
         <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
           <Container
@@ -112,8 +141,16 @@ const handleChangeImage = (event) => {
                 <Form.File 
                     id='image-upload' 
                     accept='image/*'
-                    onChange={handleChangeImage}/>
+                    onChange={handleChangeImage}
+                    ref={imageInput}
+                    />
+                    
             </Form.Group>
+            {errors?.image?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+                ))}
             <div className="d-md-none">{textFields}</div>
           </Container>
         </Col>
